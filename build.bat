54633ce00aa107bc65c53b0708f6de0c25aed3d0
Doc0160 <t0160d@gmail.com>
2016-12-01 15:57:34 +0100

    yah

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 @echo off
 cd D:\___\Party-Junkie\html
 git rm *
-cd D:\___\Party-Junkie\
+cd D:\___\Party-Junkie\php
 php php/generateCustom.php
+cd D:\___\Party-Junkie\
 git add *
 git commit -m "yah"
 git push
