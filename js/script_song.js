f642fac9c8c92ebf5371b8f6f14425fe3d77af70
Doc0160 <t0160d@gmail.com>
2016-12-17 11:19:36 +0100

    11:19:36,72 17/12/2016

--- js/script_song.js
+++ js/script_song.js
@@ -3,35 +3,35 @@ $(document).ready(function(){
     var ec = new Evercookie();
 	var song = new Audio('https://doc0160.github.io/Party-Junkie/song/marioTheme.mp3');
     $(song).onbeforeunload = function(){
-        ec.setItem("current_time", song.currentTime);
+        ec.set("current_time", song.currentTime);
         alert(song.currentTime);
     };
     if(!('volume' in ec)) {
-        ec.setItem("volume", 0.1);
+        ec.set("volume", 0.1);
     }
     if(!('mute' in ec)) {
-        ec.setItem("mute", "false");
+        ec.set("mute", "false");
     }
     if(!('current_time' in ec)) {
-        ec.setItem("current_time", 0);
+        ec.set("current_time", 0);
     }
-    song.volume = Math.pow(parseFloat(ec.getItem("volume")), 2.0);
+    song.volume = Math.pow(parseFloat(ec.get("volume")), 2.0);
     song.loop = true;
     
     var pl = function(){
         $('#svg_song').removeClass('invisible');
         $('#svg_mute').addClass('invisible');
-        ec.setItem("mute", "false");
+        ec.set("mute", "false");
         song.play();
     };
     var pa = function() {
-        ec.setItem("current_time", song.currentTime);
+        ec.set("current_time", song.currentTime);
         $('#svg_song').addClass('invisible');
         $('#svg_mute').removeClass('invisible');
-        ec.setItem("mute", "true");
+        ec.set("mute", "true");
         song.pause();
     };
-    if(ec.getItem("mute") == "false") {
+    if(ec.get("mute") == "false") {
         pl();
     } else {
         pa();
@@ -54,12 +54,12 @@ $(document).ready(function(){
 	    var detail = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
 	    
 	    if(detail > 0){
-		    ec.setItem("volume", Math.min(1, parseFloat(ec.getItem("volume")) + 0.025));
+		    ec.set("volume", Math.min(1, parseFloat(ec.get("volume")) + 0.025));
 	    } else {
-		    ec.setItem("volume", Math.max(0, parseFloat(ec.getItem("volume")) - 0.025));
+		    ec.set("volume", Math.max(0, parseFloat(ec.get("volume")) - 0.025));
 	    }
-		console.log("Volume set to: " + ec.getItem("volume"));
-        song.volume = Math.pow(parseFloat(ec.getItem("volume")), 2.0);
+		console.log("Volume set to: " + ec.get("volume"));
+        song.volume = Math.pow(parseFloat(ec.get("volume")), 2.0);
         e.preventDefault();
     }
 });
