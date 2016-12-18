efd9c2bd0288de2bcc907601e9d60b6b70db154a
Doc0160 <t0160d@gmail.com>
2016-12-18 10:46:51 +0100

    10:46:51,85 18/12/2016

--- php/generateCustom.php
+++ php/generateCustom.php
@@ -2,29 +2,6 @@
 
 require('generateCustomFunc.php');
 
-/*<?xml version="1.0" encoding="utf-8"?>
-<feed xmlns="http://www.w3.org/2005/Atom">
-
-<title>Fil d'exemple</title>
- <subtitle>Un titre secondaire.</subtitle>
- <link href="http://example.org/"/>
- <updated>2010-05-13T18:30:02Z</updated>
- <author>
-   <name>Paul Martin</name>
-   <email>paulmartin@example.com</email>
- </author>
- <id>urn:uuid:60a76c80-d399-11d9-b91C-0003939e0af6</id>
-
- <entry>
-   <title>Des robots propulsés par Atom deviennent fous</title>
-   <link href="http://example.org/2003/12/13/atom03"/>
-   <id>urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a</id>
-   <updated>2010-04-01T18:30:02Z</updated>
-   <summary>Poisson d'avril !</summary>
-</entry>
-
-   </feed>*/
-//date("F d Y H:i:s.", filemtime($filename))
 $feed = "<?xml version=\"1.0\" encoding=\"utf-8\"?>
 <feed xmlns=\"http://www.w3.org/2005/Atom\">
 
@@ -93,7 +70,7 @@ for ($a = $nb -1; $a > 1; $a--) {
     while ($i < $nb) {
 		$temp = explode("-", $name[$i]);
 	    $page .= '<li>'.
-                 '<a href="'.make_html_filename_from_xml_filename($name[$i]).'">'.
+                 '<a href="'.make_num_from_xml_filename($name[$i]).'">'.
 				 '<span class="numbers">'.$temp[0]."</span>".
                  ' - '.
                  make_title_from_xml_filename($name[$i]).
